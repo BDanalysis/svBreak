3236da1ff9909538ec1440b569ae763f0c959f8f
BDanalysis <41245376+BDanalysis@users.noreply.github.com>
2021-03-04 22:53:49 +0800

    Delete svBreakCode directory

--- svBreakCode/preprocessing/delete100M.py
+++
@@ -1,5 +0,0 @@
-with open('/home/shaoqiangwang/BREAKPOINT/test/all_mutation_10_24.sam', 'r') as f:
-    with open('/home/shaoqiangwang/BREAKPOINT/test/all_mutation_10_24_input.sam', 'w') as g:
-        for line in f.readlines():
-            if '100M' not in line:
-                g.write(line)
--- svBreakCode/preprocessing/deleteN.py
+++
@@ -1,8 +0,0 @@
-import shutil
-with open('/home/shaoqiangwang/BREAKPOINT/chr21.fa', 'r') as f:
-    with open('/home/shaoqiangwang/BREAKPOINT/all_mutation_original.fa', 'w') as g:
-        for line in f.readlines():
-            if 'N' not in line:
-                g.write(line)
-#shutil.move('/home/shaoqiangwang/BREAKPOINT/all_mutation_original.fa', '/home/shaoqiangwang/BREAKPOINT/chr21.fa')
-#去掉注释将直接覆盖掉原来的chr21.fa，并删除N
