3b873e696e637d0ad27e819a43cededd6364486d
BDanalysis <41245376+BDanalysis@users.noreply.github.com>
2021-03-04 23:50:46 +0800

    Delete run directory

--- run/runTerminal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-fasta=$1;
-fastq1=$2;
-fastq2=$3;
-output=$4
-property=$5;
-chr=$6;
-
-java -classpath ../svBreak/target/classes com.xie.detection.MAIN $fasta $fastq1 $fastq2 $output $property $chr
